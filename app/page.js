70e6d5c4e7dd776a4dac290db40b7d3d00a535bc
Volodymyr O <hello@vdigital.design>
2024-11-21 16:18:27 +0200

    Props

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
-const Heading = () => {
-    const Title = "Hello Page";
-    return <h2>{Title}</h2>
+const Heading = ({title, description}) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <span>{description}</span>
+        </div>
+    )
 }
 
 export default function Page() {
+    const myTitle = "Hello Page!!!";
+    const myDescription = "This is a React Primer ...";
+
     return (
         <div>
-            <Heading />
+            <Heading title={myTitle} description={myDescription} />
             <p>Some content ...</p>
         </div>
     );
